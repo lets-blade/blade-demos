0198eb0852adbfc6df4d6555b6f5ad18b17188c7
biezhi <biezhi.me@gmail.com>
2017-12-15 18:12:06 +0800

    :heavy_plus_sign: add custom banner module

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 blade version => `2.0.3`
 
 - [blade-helloworld](blade-helloworld)
+- [blade-upload](blade-upload)
+- [blade-ajax](blade-ajax)
 - [blade-route](blade-route)
 - [blade-template](blade-template)
 - [blade-vue-todolist](blade-vue-todolist)
 - [blade-package](blade-package)
+- [blade-custom-banner](blade-custom-banner)
---
+++ blade-custom-banner/src/main/java/com/blade/demo/custom/CustomApplication.java
@@ -0,0 +1,18 @@
+package com.blade.demo.custom;
+
+import com.blade.Blade;
+
+/**
+ * @author biezhi
+ * @date 2017/12/15
+ */
+public class CustomApplication {
+
+    public static void main(String[] args) {
+        Blade.me()
+                .bannerText("Hei hei hei~~~")
+                .threadName("(* ￣3)(ε￣ *)")
+                .start(CustomApplication.class, args);
+    }
+
+}
