374f797a55b368414620ea7a23d55e705d7fd781
biezhi <biezhi.me@gmail.com>
2018-12-16 20:53:33 +0800

    :arrow_up: upgrade blade

--- blade-static/src/main/java/com/bladejava/staticdir/StaticApplication.java
+++ blade-static/src/main/java/com/bladejava/staticdir/StaticApplication.java
@@ -3,13 +3,14 @@ package com.bladejava.staticdir;
 import com.blade.Blade;
 
 /**
+ *
  * @author biezhi
  * @date 2018/6/1
  */
 public class StaticApplication {
 
     public static void main(String[] args) {
-        Blade.of().start(StaticApplication.class, args);
+         Blade.of().start(StaticApplication.class, args);
     }
 
 }
